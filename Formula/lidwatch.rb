7d82a327f86537c2bd1ec8ca80598f63e416022e
Mustafa Eyceoz <maxusmusti@gmail.com>
2026-07-23 16:32:03 -0400

    Update formula SHA256 for v0.1.0 release

--- Formula/lidwatch.rb
+++ Formula/lidwatch.rb
@@ -2,7 +2,7 @@ class Lidwatch < Formula
   desc "Keep your Mac awake while AI agents run"
   homepage "https://github.com/Maxusmusti/lidwatch"
   url "https://github.com/Maxusmusti/lidwatch/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   version "0.1.0"
 
